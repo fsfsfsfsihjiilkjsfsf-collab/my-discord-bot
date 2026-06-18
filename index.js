09904ea74da0c9efd910252014ee9a46653b4061
fsfsfsfsihjiilkjsfsf-collab <fsfsfsfsihjiilkjsfsf@gmail.com>
2026-06-18 12:34:46 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+  console.log(`บอทออนไลน์แล้วในชื่อ: ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
